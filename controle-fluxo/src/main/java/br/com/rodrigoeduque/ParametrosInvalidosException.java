9befd43b1cb0878b17106a7880bf2cc1afb5df0b
Rodrigo Eustáquio Duque <rodrigoeduque@hotmail.com>
2024-12-14 14:40:57 -0300

    entrega-desafio

---
+++ controle-fluxo/src/main/java/br/com/rodrigoeduque/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package br.com.rodrigoeduque;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
